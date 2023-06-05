de24a579b8892f5290581c2fab43a090baed91b0
Ronlin1 <ronlinx6@gmail.com>
2023-06-05 10:07:08 +0300

    push g4 changes

--- Todo/settings.py
+++ Todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d!%)2^9c-jb1v0)%v=+l(k5@cio$_2wnxw=n*!g*26it)8zqg&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
