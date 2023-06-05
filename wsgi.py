bb95395ddd1df96ced3ee00ed7e641114f2414f4
Ronlin1 <ronlinx6@gmail.com>
2023-06-05 10:13:51 +0300

    push app wsgu changes

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from waitress import serve
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todolistprj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Todo.settings')
 
 application = get_wsgi_application()
 
